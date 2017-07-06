b4293b82cc6c4101c74117efe62fb5fe0a143463
canhnht <canhnht1709@gmail.com>
2017-07-06 15:49:23 +0700

    Update port

--- index.js
+++ index.js
@@ -120,6 +120,8 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+// console.log(process.env.PORT);
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
